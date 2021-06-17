e712a2b15b50b6f4873649bc25d6167d76e46579
Jelena Bozinovic <jekica87@gmail.com>
2021-06-17 21:41:01 +0200

    Update BasicPage and LocationPopupPage, add testNG

--- src/pages/BasicPage.java
+++ src/pages/BasicPage.java
@@ -1,5 +1,11 @@
 package pages;
 
-public class BasicPage {
+import org.openqa.selenium.WebDriver;
 
+public abstract class BasicPage {
+	protected WebDriver driver;
+
+	public BasicPage(WebDriver driver) {
+		this.driver = driver;
+	}
 }
--- src/pages/LocationPopupPage.java
+++ src/pages/LocationPopupPage.java
@@ -1,5 +1,22 @@
 package pages;
 
-public class LocationPopupPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class LocationPopupPage extends BasicPage {
+
+	public LocationPopupPage(WebDriver driver) {
+		super(driver);
+	}
+	
+	//get metoda za element koji prikazuje lokaciju u hederu 
+
+	public WebElement getHeaderLocation() {
+		return driver.findElement(By.className("location-selector"));
+	}
+
+	public WebElement getClose() {
+		return driver.findElement(By.className("close-btn close-btn-white"));
+	}
 }
