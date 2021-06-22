9503de7047a4882755d09496c08d9b3694683e5f
Jelena Bozinovic <jekica87@gmail.com>
2021-06-22 22:17:54 +0200

    Update SearchResultPage

--- src/pages/SearchResultPage.java
+++ src/pages/SearchResultPage.java
@@ -1,12 +1,42 @@
 package pages;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SearchResultPage extends BasicPage {
 
 	public SearchResultPage(WebDriver driver, JavascriptExecutor js) {
-		super(driver,js);
+		super(driver, js);
+
+	}
+
+	// Method for all search results
+
+	public List<WebElement> getSearchResults() {
+		return driver.findElements(By.xpath("//*[@class='product-name']/a"));
+	}
+
+	// Method for all search meal titles
+
+	public ArrayList<String> getAllResultsNames() {
+		ArrayList<String> names = new ArrayList<>();
+
+		for (int i = 0; i < getSearchResults().size(); i++) {
+			String name = getSearchResults().get(i).getText();
+			names.add(name);
+		}
+		return names;
+	}
+
+	// Method for result number
+
+	public int getResultsNumber() {
+		return getSearchResults().size();
 	}
 
 }
